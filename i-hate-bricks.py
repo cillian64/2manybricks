a4299dc99bc635ff07b4593ba246b83fe7d9ef4b
David Turner <cillian64@googlemail.com>
2016-09-23 23:33:14 +0100

    Bouncing off the outside of a rectangle.

--- i-hate-bricks.py
+++ i-hate-bricks.py
@@ -4,10 +4,10 @@ from math import sin, cos, pi
 
 size = width, height = 1024, 768
 screen = pygame.display.set_mode(size)
-ball_defaults = {'position': [int(width/2), int(height/2)],
-                 'bearing': -1.3,
-                 'speed': 5,
-                 'radius': 10,
+ball_defaults = {'position': [50, 50],
+                 'bearing': -0.7,
+                 'speed': 10,
+                 'radius': 50,
                  'colour': (255, 0, 0)
                  }
 background = (0, 0, 0)
@@ -35,44 +35,98 @@ class Ball:
         self.position[0] += int(self.speed * cos(self.bearing))
         self.position[1] += int(self.speed * sin(self.bearing))
 
-    def collide_rect(self, rect, internal):
+    def condition_bearing(self):
         """
-        rect is a pygame Rect.
-        internal says whether we are bouncing around the inside of the
-        rectange, or are outside it bouncing agaist it.
+        Ensure the bearing is in the range -pi to pi
         """
-        left, top = rect.left, rect.top
-        right = rect.left + rect.width
-        bottom = rect.top + rect.height
-        if internal:
-            if self.position[1] - top < self.radius:
-                self.bearing *= -1
-                self.position[1] = top + self.radius
-            if bottom - self.position[1] < self.radius:
-                self.bearing *= -1
-                self.position[1] = bottom - self.radius
-            if self.position[0] - left < self.radius:
-                self.bearing = self.bearing * -1 + pi
-                self.position[0] = left + self.radius
-            if right - self.position[0] < self.radius:
-                self.bearing = self.bearing * -1 + pi
-                self.position[0] = right - self.radius
-        else:  # external
-            raise NotImplementedError
-
-        # Condition bearing back into the range -pi to +pi
         while self.bearing < -pi:
             self.bearing += 2*pi
         while self.bearing > pi:
             self.bearing -= 2*pi
 
-        assert isinstance(self.position[0], int)
-        assert isinstance(self.position[1], int)
+    def collide_rect_internal(self, rect):
+        """
+        rect is a pygame Rect.
+        Use this method when we are bouncing around inside the rectangle
+        """
+        if self.position[1] - rect.top < self.radius:
+            # Bounce top edge
+            self.bearing *= -1
+            self.position[1] = rect.top + self.radius
+        if rect.bottom - self.position[1] < self.radius:
+            # Bounce bottom edge
+            self.bearing *= -1
+            self.position[1] = rect.bottom - self.radius
+        if self.position[0] - rect.left < self.radius:
+            # Bounce left edge
+            self.bearing = self.bearing * -1 + pi
+            self.position[0] = rect.left + self.radius
+        if rect.right - self.position[0] < self.radius:
+            # Bounce right edge
+            self.bearing = self.bearing * -1 + pi
+            self.position[0] = rect.right - self.radius
+        self.condition_bearing()
+
+    def collide_rect_external(self, rect):
+        """
+        rect is a pygame Rect.
+        Use this method when we are bouncing against the outside of a rectangle
+        """
+        # If we end up inside the rectangle this collision detection breaks
+        # down horribly.
+        if (self.position[0] > rect.left and self.position[0] < rect.right
+                and self.position[1] > rect.top
+                and self.position[1] < rect.bottom):
+            raise RuntimeError("Cannot resolve external collision with a ball "
+                               "inside a rectangle.")
+
+        # Collision between a ball and the outside of a rectangle is
+        # surprisingly complicated.  In the simple 2d simulation the incident
+        # angle equals the reflected angle, both normal to the reflecting
+        # plane.  This breaks down on a corner, because the angle of the
+        # reflecting plane is undefined.  So instead we just simulate the
+        # centre point of the ball reflecting off a rectangle enlarged by the
+        # radius of the circle.
+        # A more interesting simulation would be to give the enlarged rectangle
+        # radiused corners, with the same radius as the ball.  Then you get
+        # interesting reflection angles if the ball reflects off the corner of
+        # the rectangle.  It's unclear how canonical Breakout handles this.
+
+        # Check if we should collide against the top or bottom edges
+        if (self.position[0] > rect.left - self.radius
+                and self.position[0] < rect.right + self.radius):
+            if (rect.top - self.position[1] < self.radius
+                    and rect.top - self.position[1] > 0):
+                # Bounce top edge
+                self.bearing *= -1
+                self.position[1] = rect.top - self.radius
+            elif (self.position[1] - rect.bottom < self.radius
+                    and self.position[1] - rect.bottom > 0):
+                # Bounce bottom edge
+                self.bearing *= -1
+                self.position[1] = rect.bottom + self.radius
+
+        # Check if we should collide against the left or right edges
+        if (self.position[1] > rect.top - self.radius
+                and self.position[1] < rect.bottom):
+            if (rect.left - self.position[0] < self.radius
+                    and rect.left - self.position[0] > 0):
+                # Bounce left edge
+                self.bearing = self.bearing * -1 + pi
+                self.position[0] = rect.left - self.radius
+            elif (self.position[0] - rect.right < self.radius
+                    and self.position[0] - rect.left > 0):
+                # Bounce right edge
+                self.bearing = self.bearing * -1 + pi
+                self.position[0] = rect.right + self.radius
+        self.condition_bearing()
 
 
 balls = []
 balls.append(Ball(**ball_defaults))
 screen_rect = pygame.Rect(0, 0, width, height)
+
+box = pygame.Rect(300, 300, 300, 300)
 
 while True:
     for event in pygame.event.get():
@@ -80,8 +134,10 @@ while True:
             sys.exit()
 
     balls[0].move()
-    balls[0].collide_rect(screen_rect, internal=True)
+    balls[0].collide_rect_internal(screen_rect)
+    balls[0].collide_rect_external(box)
 
     screen.fill(background)
+    pygame.draw.rect(screen, (255, 255, 255), box)
     balls[0].draw(screen)
     pygame.display.flip()

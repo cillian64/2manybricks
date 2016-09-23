d30e859660a3b06bfef05330b375eab253f58594
David Turner <cillian64@googlemail.com>
2016-09-24 00:26:08 +0100

    Added paddle

--- i-hate-bricks.py
+++ i-hate-bricks.py
@@ -3,6 +3,7 @@ import pygame
 
 from ball import Ball
 from brick import Brick
+from paddle import Paddle
 
 size = width, height = 1024, 768
 screen_rect = pygame.Rect(0, 0, width, height)
@@ -21,10 +22,16 @@ balls.append(Ball(**ball_defaults))
 bricks = []
 bricks.append(Brick(pygame.Rect(300, 300, 300, 300), (255, 255, 255), 1))
 
+paddle = Paddle(pygame.Rect(100, height-50, 150, 15), width, (255, 255, 255))
+
 while True:
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    # Move the paddle
+    paddle.move(pygame.mouse.get_pos())
 
     # Handle all the ball movement and collision
     for ball in balls:
@@ -32,6 +39,7 @@ while True:
         for brick in bricks:
             if ball.collide_rect_external(brick.rect):
                 brick.hit()
+        paddle.collide(ball)
         balls[0].move()
 
     # Remove dead bricks
@@ -43,4 +51,6 @@ while True:
         brick.draw(screen)
     for ball in balls:
         ball.draw(screen)
+    paddle.draw(screen)
+
     pygame.display.flip()
---
+++ paddle.py
@@ -0,0 +1,47 @@
+import pygame
+from math import pi
+
+
+class Paddle:
+    def __init__(self, rect, width, colour):
+        """
+        rect is a pygame rect describing my position and size.  I'll keep the
+        shape and Y-position forever, but am open to updates in x!
+        width is the width of the screen, so I know how to not go off teh edges
+        colour is a tuple of (red, green, blue)
+        """
+        self.width = rect.width
+        self.height = rect.height
+        self.x = rect.left + int(rect.width / 2)
+        self.y = rect.top + int(rect.height / 2)
+        self.screen_width = width
+        self.colour = colour
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.colour, self.rect())
+
+    def move(self, pos):
+        """
+        pos is the result of pygame.mouse.pos, a tuple of (x,y)
+        I will ignore the Y coordinate, and take the X coordinate under
+        suggestion, so I don't go off the edge of the screen.
+        """
+        self.x = pos[0]
+        if self.x < int(self.width / 2):
+            self.x = int(self.width / 2)
+        if self.x > self.screen_width - int(self.width / 2):
+            self.x = self.screen_width - int(self.width / 2)
+
+    def rect(self):
+        return pygame.Rect(self.x - int(self.width / 2),
+                           self.y - int(self.height / 2),
+                           self.width, self.height)
+
+    def collide(self, ball):
+        if (ball.position[0] > self.x - self.width/2 - ball.radius
+                and ball.position[0] < self.x + self.width/2 + ball.radius
+                and ball.position[1] > self.y - self.height/2 - ball.radius
+                and ball.position[1] < self.y + self.height/2):
+            siding = (self.x - ball.position[0]) / self.width
+            ball.bearing = -pi/2 - pi/2 * siding
+            ball.position[1] = self.y - int(self.height/2) - ball.radius - 1
